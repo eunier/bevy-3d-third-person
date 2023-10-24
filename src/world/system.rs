ef4c716488da86cd0c432ce5f7f680ffe75da123
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-23 21:33:51 -0400

    floor

--- src/world/system.rs
+++ src/world/system.rs
@@ -5,6 +5,11 @@ pub fn spawn_floor(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    let floor = PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane::from_size(15.0))),
+        material: materials.add(Color::DARK_GRAY.into()),
+        ..default()
+    };
 
-    //
+    commands.spawn(floor);
 }
