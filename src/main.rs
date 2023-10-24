21ad0a080460e1ed543a0fe87f104ff8099220a4
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-23 20:39:44 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 mod world;
 
 use bevy::prelude::*;
-use world::system::spawn_floor;
 
 fn main() {
-    spawn_floor();
     App::new().add_plugins(DefaultPlugins).run();
 }
--- src/world/system.rs
+++ src/world/system.rs
@@ -1,9 +1,10 @@
 use bevy::prelude::*;
 
 pub fn spawn_floor(
-    // mut commands: Commands,
-    // mut meshes: ResMut<Assets<Mesh>>,
-    // mut materials: ResMut<Assets<StandardMaterial>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+
     //
 }
