714ab5f76e07933c258c5ad0a1a8df753c13cde1
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-23 21:39:21 -0400

    add camera

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod camera;
 mod world;
 
 use bevy::prelude::*;
+use camera::plugin::CameraPlugin;
+use world::plugin::WorldPlugin;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins((CameraPlugin, WorldPlugin, DefaultPlugins))
+        .run();
 }
